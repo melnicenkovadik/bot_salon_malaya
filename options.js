8c39af3563d77cfe2052b8daf0d579a2cf2ce5a4
melnicenkovadik <melnicenkovadik@gmail.com>
2021-05-13 21:03:56 +0300

    initial commit

---
+++ options.js
@@ -0,0 +1,39 @@
+module.exports = {
+    masterOptions: {
+        reply_markup: JSON.stringify({
+            inline_keyboard: [
+                [{text: 'Мастер маникюра', callback_data: '/manikMaster'}],
+                [{text: 'Парикхмахер', callback_data: '/hairMaster'}]
+            ]
+        })
+    },
+    manikOptions: {
+        reply_markup: JSON.stringify({
+            inline_keyboard: [
+                [{text: '10:00', callback_data: '10:00'}, {text: '12:00', callback_data: '12:00'}, {
+                    text: '14:00',
+                    callback_data: '14:00'
+                }],
+                [{text: 'Написать Алине', callback_data: 'alinkaslz'}],
+            ]
+        })
+    },
+    hairOptions: {
+        reply_markup: JSON.stringify({
+            inline_keyboard: [
+                [{text: '10:00', callback_data: '10:00'}, {text: '12:00', callback_data: '12:00'}, {
+                    text: '14:00',
+                    callback_data: '14:00'
+                }],
+                [{text: 'Написать Маше', callback_data: 'mariaArinina'}],
+            ]
+        })
+    },
+    menu: {
+        reply_markup: JSON.stringify({
+            inline_keyboard: [
+                [{text: 'Продолжить', callback_data: '/appointment'}],
+            ]
+        })
+    }
+}
